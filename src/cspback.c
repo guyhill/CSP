444ba8d8ac4cd394f8a93f60c3e25c96bccbe0f1
Guido van den Heuvel <g.vandenheuvel@cbs.nl>
2026-06-02 11:09:28 +0200

    Removed hitherto undetected unused #include

--- src/cspback.c
+++ src/cspback.c
@@ -16,7 +16,7 @@
 
 #include <stdlib.h>
 #include "cspback.h"
-#include "cspmain.h"
+//#include "cspmain.h"
 
 static int (*external_stop)(void) = {NULL};
 static int (*external_heur)(void) = {NULL};
